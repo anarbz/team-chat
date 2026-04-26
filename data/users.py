02b394cd5f136f294581e3169b7971319819e35e
anar <anarhbairamov@ya.ru>
2026-04-26 14:50:15 +0300

    модели - users, messages - пока не связаны между собой - для тестирования

---
+++ data/users.py
@@ -0,0 +1,16 @@
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+from datetime import datetime
+
+
+class User(SqlAlchemyBase):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    login = sqlalchemy.Column(sqlalchemy.String, unique=True, nullable=False)
+    about = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    email = sqlalchemy.Column(sqlalchemy.String, unique=True, nullable=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                     default=datetime.now)
