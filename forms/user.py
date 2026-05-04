07488a4cc1cae26cacc0d3393481c5683ce219da
NotDox <rodicheva99@list.ru>
2026-05-04 19:28:10 +0300

    1 часть. надеюсь правильно все сделала, проверить пока не могуп. user model, auth forms and templates

---
+++ forms/user.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Email, EqualTo
+
+class RegisterForm(FlaskForm):
+    login = StringField('Логин', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password_again = PasswordField('Повторите пароль', validators=[DataRequired(), EqualTo('password', message='Пароли должны совпадать')])
+    submit = SubmitField('Зарегистрироваться')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
