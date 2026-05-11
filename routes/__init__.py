51e1932f487eac475fed7b443684cd2493cbe933
anar <anarhbairamov@ya.ru>
2026-05-11 18:20:02 +0300

    добавил маршрут списка чатов

--- routes/__init__.py
+++ routes/__init__.py
@@ -2,5 +2,6 @@ from .auth import auth_bp
 from .dashboard import dashboard_bp
 from .enter import enter_bp
 from .chat_routes import chat_bp
+from .chat_list import chats_list_bp
 
-__all__ = ['auth_bp', 'dashboard_bp', 'enter_bp', 'chat_bp']
+__all__ = ['auth_bp', 'dashboard_bp', 'enter_bp', 'chat_bp', 'chats_list_bp']
---
+++ routes/chat_list.py
@@ -0,0 +1,29 @@
+from flask import Blueprint, render_template
+from flask_login import login_required, current_user
+
+from data import db_session
+from data.chats import Chat
+from data.chat_members import ChatMember
+
+
+chats_list_bp = Blueprint('chats_list', __name__)
+
+
+@chats_list_bp.route('/chats')
+@login_required
+def chats():
+    # получаем все чаты текущего пользователя
+    db_sess = db_session.create_session()
+
+    memberships = db_sess.query(ChatMember).filter(
+        ChatMember.user_id == current_user.id).all()
+
+    chat_ids = [membership.chat_id for membership in memberships]
+
+    if chat_ids:
+        user_chats = db_sess.query(Chat).filter(Chat.id.in_(chat_ids)).all()
+    else:
+        user_chats = []
+    db_sess.close()
+
+    return render_template('chats.html', chats=user_chats)
